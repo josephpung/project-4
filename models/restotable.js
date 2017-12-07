ab686504ce8a6d8b3b432fbfa4274b77da8aa65a
josephpung <goldenpummel@live.com>
2017-12-07 14:13:26 +0800

    .

--- models/restotable.js
+++ models/restotable.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt')
 const restotableSchema = new Schema({
   user_id: String,
   restaurant_id: String,
+  transaction_id: String,
   table_number: String,
   dishes: Object,
   status: {type: String, default: "Preparing"}
