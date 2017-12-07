f7f60e10e8bb5a820ddcc54d46ba72e33b1fd282
josephpung <goldenpummel@live.com>
2017-12-07 14:28:16 +0800

    ,

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -21,7 +21,7 @@ class Header extends Component {
           <li><NavLink to="/payment">Payment</NavLink></li>
           <li><NavLink to="/tables">View Tables</NavLink></li>
           <li><NavLink to="/kitchen">View Kitchen</NavLink></li>
-          <li><NavLink to="/menu">View Menu</NavLink></li>
+          <li><NavLink to="/custmenu">View Menu</NavLink></li>
           <li><NavLink to="/admin_console">Admin</NavLink></li>
           <li><NavLink to="/" onClick={(e)=>{this.handleClick(e)}}>logout</NavLink></li>
           </Navbar>
@@ -36,7 +36,7 @@ class Header extends Component {
           <li><NavLink to="/payment">Payment</NavLink></li>
           <li><NavLink to="/tables">View Tables</NavLink></li>
           <li><NavLink to="/kitchen">View Kitchen</NavLink></li>
-          <li><NavLink to="/menu">View Menu</NavLink></li>
+          <li><NavLink to="/custmenu">View Menu</NavLink></li>
           <li><NavLink to="/admin_console">Admin</NavLink></li>
 
 
